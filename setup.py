2283d2153afad4b7e5f5eb7abd9e92ae1d224655
ismail-bc <142583248+ismail-bc@users.noreply.github.com>
2024-03-16 11:48:21 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,30 +2,25 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '0.0.1'
 DESCRIPTION = 'Converting SMILES to a list of numbers'
 LONG_DESCRIPTION = 'A package that allows that Converts SMILES to a list of numbers that gives every unique atom and the atoms connected to in a number.'
 
 # Setting up
 setup(
-    name="vidstream",
+    name="ismchem",
     version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
+    author="ismail-bc (Ismail Khalid)",
+    author_email="<ismail.bootcamp@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=['rdkit', 'copy'],
+    keywords=['python', 'chemistry', 'SMILES', 'data science],
     classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
+        "Development Status :: 1 - Release",
+        "Intended Audience :: Chemists/Data-scientist",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
